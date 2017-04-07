56a5c3e4f15f940a5e9b5d0902829ec04647ef72
David Shin <xdavidshinx@gmail.com>
2017-04-07 11:36:00 -0700

    add checkboxes and working submit button

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault()
+
+    var defaultAmount = 0;
+    var premiumAmount = 0;
+    var totalPizzaCost = 0;
+    var size = $("#size").val();
+
+  //constructor functions
+    function defaultTopping(size) {
+      this.topping = topping;
+    }
+
+    function premiumTopping(topping) {
+      this.topping = topping;
+    }
+  //prototype functions
+    defaultTopping.prototype.defaultCount = $("input:checkbox[name=topping1]:checked").each(function(){
+        defaultAmount+=1
+      });
+
+
+    premiumTopping.prototype.defaultCount = $("input:checkbox[name=topping2]:checked").each(function(){
+        premiumAmount+=1
+      });
+
+    //pricing functions. Accounts for 1 free default topping
+    function totalPrice(){
+      if(size==="Small"){
+        if(defaultAmount>0){
+        totalPizzaCost+=(8+(defaultAmount*2-2)+(premiumAmount*3));
+        return totalPizzaCost
+      }else{
+        totalPizzaCost+=(8+(defaultAmount*2)+(premiumAmount*3));
+        return totalPizzaCost
+        }
+      }if(size==="Medium"){
+        if(defaultAmount>0){
+        totalPizzaCost+=(11+(defaultAmount*2-2)+(premiumAmount*3));
+        return totalPizzaCost
+      }else{
+        totalPizzaCost+=(11+(defaultAmount*2)+(premiumAmount*3));
+        return totalPizzaCost
+        }
+      }if(size==="Large"){
+        if(defaultAmount>0){
+        totalPizzaCost+=(14+(defaultAmount*2-2)+(premiumAmount*3));
+        return totalPizzaCost
+      }else{
+        totalPizzaCost+=(14+(defaultAmount*2)+(premiumAmount*3));
+        return totalPizzaCost
+        }
+      }
+    }
+    $("#receipt").text("Your Total Cost: $" + totalPrice());
+  });
+});

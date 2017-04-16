2ef5741e86c63dd3091af7ebf516f3cb4e56d618
David Shin <xdavidshinx@gmail.com>
2017-04-16 14:30:48 -0700

    bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-function pizza(size,defaultTopping,premiumTopping,totalPizzaCost){
+function pizza(size,defaultTopping,premiumTopping){
   this.size = size;
   this.defaultTopping = defaultTopping;
   this.premiumTopping = premiumTopping;
-  this.totalPizzaCost = totalPizzaCost;
 }
 
 var defaultToppingsArray = [];
@@ -30,36 +29,26 @@ function premiumPush() {
 }
 
 pizza.prototype.price = function() {
-  var total = 0
-  if(size==="Small ($8)"){
+  var total = 0;
+  if(this.size==="Small ($8)"){
     if(this.defaultTopping.length>0){
     total+=(8+(this.defaultTopping.length*2-2)+(this.premiumTopping.length*3));
-    return total;
   }else{
     total+=(8+(this.defaultTopping.length*2)+(this.premiumTopping.length*3));
-    return total;
     }
-  }if(size==="Medium ($11)"){
+  }if(this.size==="Medium ($11)"){
     if(this.defaultTopping.length>0){
     total+=(11+(this.defaultTopping.length*2-2)+(this.premiumTopping.length*3));
-    return total;
   }else{
     total+=(11+(this.defaultTOpping.length*2)+(this.premiumTopping.length*3));
-    return total;
     }
-  }if(size==="Large ($14)"){
+  }if(this.size==="Large ($14)"){
     if(this.defaultTopping.length>0){
     total+=(14+(this.defaultTopping*2-2)+(this.premiumTopping*3));
-    return total;
   }else{
     total+=(14+(this.defaultTopping*2)+(this.premiumTopping*3));
-    return total;
     }
-  }
-}
-
-pizza.prototype.premiumCount = function() {
-  return pizza.premiumTopping.length
+  }return total;
 }
 
 $(document).ready(function(){
@@ -72,12 +61,15 @@ $(document).ready(function(){
     premiumToppingsArray = [];
 
     var size = $("#size").val();
-    var newPizza = new pizza(size,defaultToppingsArray,premiumToppingsArray,0);
-    console.log(newPizza.price())
-    $("#textAppend").text("Your Total Cost: $" + newPizza.price());
+
+    var newPizza = new pizza(size,defaultToppingsArray,premiumToppingsArray);
 
     defaultPush();
     premiumPush();
+
+    console.log(newPizza.defaultTopping)
+    console.log(newPizza.price())
+    $("#textAppend").text("Your Total Cost: $" + newPizza.price());
     appendDefault();
   });
 });

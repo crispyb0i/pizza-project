e79cdb8b604b1a637e8870bb27cc0f068bb1a088
David Shin <xdavidshinx@gmail.com>
2017-04-16 13:12:12 -0700

    Seperated business and ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,58 +8,27 @@ function pizza(size,defaultTopping,premiumTopping,totalPizzaCost){
 var defaultToppingsArray = [];
 var premiumToppingsArray = [];
 
-//pricing functions. Accounts for 1 free default topping
-
-
-    //list out toppings in receipt
-    function appendDefault(){
-      for(var i=0;i<defaultToppingsArray.length;i++){
-        $("ul#defaultToppingsReceipt").append("<li>" + defaultToppingsArray[i] + "</li>");
-      }
-      for(var i=0;i<premiumToppingsArray.length;i++){
-        $("ul#premiumToppingsReceipt").append("<li>" + premiumToppingsArray[i] + "</li>")
-      }
-    }
-
-    function totalPrice(){
-      if(this.size==="Small ($8)"){
-        if(newPizza.defaultTopping.length>0){
-        newPizza.totalPizzaCost+=(8+(newPizza.defaultTopping.length*2-2)+(newPizza.premiumTopping.length*3));
-        return newPizza.totalPizzaCost
-      }else{
-        newPizza.totalPizzaCost+=(8+(newPizza.defaultTopping.length*2)+(newPizza.premiumTopping.length*3));
-        return newPizza.totalPizzaCost
-        }
-      }if(this.size==="Medium ($11)"){
-        if(newPizza.defaultTopping.length>0){
-        newPizza.totalPizzaCost+=(11+(newPizza.defaultTopping.length*2-2)+(newPizza.premiumTopping.length*3));
-        return newPizza.totalPizzaCost
-      }else{
-        newPizza.totalPizzaCost+=(11+(newPizza.defaultTOpping.length*2)+(newPizza.premiumTopping.length*3));
-        return newPizza.totalPizzaCost
-        }
-      }if(this.size==="Large ($14)"){
-        if(newPizza.defaultTopping.length>0){
-        newPizza.totalPizzaCost+=(14+(newPizza.defaultTopping*2-2)+(newPizza.premiumTopping*3));
-        return newPizza.totalPizzaCost
-      }else{
-        newPizza.totalPizzaCost+=(14+(newPizza.defaultTopping*2)+(newPizza.premiumTopping*3));
-        return newPizza.totalPizzaCost
-        }
-      }
-    }
+function appendDefault(){
+  for(var i=0;i<defaultToppingsArray.length;i++){
+    $("ul#defaultToppingsReceipt").append("<li>" + defaultToppingsArray[i] + "</li>");
+  }
+  for(var i=0;i<premiumToppingsArray.length;i++){
+    $("ul#premiumToppingsReceipt").append("<li>" + premiumToppingsArray[i] + "</li>")
+  }
+}
 
 $(document).ready(function(){
   $("#form").submit(function(event){
     event.preventDefault();
-
-
-    var size = $("#size").val();
-
     $("#receipt").show()
     $("ul").empty();
 
-    //prototype functions
+    defaultToppingsArray = [];
+    premiumToppingsArray = [];
+
+    var size = $("#size").val();
+    var newPizza = new pizza(size,defaultToppingsArray,premiumToppingsArray,0);
+
     pizza.prototype.defaultCount = $("input:checkbox[name=topping1]:checked").each(function(){
         defaultToppingsArray.push($(this).val())
       });
@@ -67,13 +36,32 @@ $(document).ready(function(){
     pizza.prototype.premiumCount = $("input:checkbox[name=topping2]:checked").each(function(){
         premiumToppingsArray.push($(this).val())
       });
-
-      totalPrice();
-
-      var newPizza = new pizza(size,defaultToppingsArray,premiumToppingsArray,totalPrice());
-      console.log(newPizza.totalPizzaCost);
-
-    $("#textAppend").text("Your Total Cost: $" + newPizza.totalPizzaCost);
+    //append total cost to receipt
+    if(size==="Small ($8)"){
+      if(newPizza.defaultTopping.length>0){
+      newPizza.totalPizzaCost+=(8+(newPizza.defaultTopping.length*2-2)+(newPizza.premiumTopping.length*3));
+      $("#textAppend").text("Your Total Cost: $" + newPizza.totalPizzaCost);
+    }else{
+      newPizza.totalPizzaCost+=(8+(newPizza.defaultTopping.length*2)+(newPizza.premiumTopping.length*3));
+      $("#textAppend").text("Your Total Cost: $" + newPizza.totalPizzaCost);
+      }
+    }if(size==="Medium ($11)"){
+      if(newPizza.defaultTopping.length>0){
+      newPizza.totalPizzaCost+=(11+(newPizza.defaultTopping.length*2-2)+(newPizza.premiumTopping.length*3));
+      $("#textAppend").text("Your Total Cost: $" + newPizza.totalPizzaCost);
+    }else{
+      newPizza.totalPizzaCost+=(11+(newPizza.defaultTOpping.length*2)+(newPizza.premiumTopping.length*3));
+      $("#textAppend").text("Your Total Cost: $" + newPizza.totalPizzaCost);
+      }
+    }if(size==="Large ($14)"){
+      if(newPizza.defaultTopping.length>0){
+      newPizza.totalPizzaCost+=(14+(newPizza.defaultTopping*2-2)+(newPizza.premiumTopping*3));
+      $("#textAppend").text("Your Total Cost: $" + newPizza.totalPizzaCost);
+    }else{
+      newPizza.totalPizzaCost+=(14+(newPizza.defaultTopping*2)+(newPizza.premiumTopping*3));
+      $("#textAppend").text("Your Total Cost: $" + newPizza.totalPizzaCost);
+      }
+    }
     appendDefault();
   });
 });
